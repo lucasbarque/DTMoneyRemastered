4ddebb2ae2e0291c617f69969ddce5d5b085b5f9
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-08-10 19:59:34 -0400

    React Hook Form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,3 +1,5 @@
+import * as z from 'zod';
+
 import * as Dialog from '@radix-ui/react-dialog';
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
@@ -8,8 +10,32 @@ import {
   TransactionType,
   TransactionTypeButton,
 } from './styles';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const newTransactionModalSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  // type: z.enum(['income', 'outcome']),
+});
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionModalSchema>;
 
 export function NewTransactionModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionModalSchema),
+  });
+
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(data);
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -21,10 +47,25 @@ export function NewTransactionModal() {
           <X size={24} />
         </CloseButton>
 
-        <form>
-          <input type='text' placeholder='Descrição' required />
-          <input type='number' placeholder='Preço' required />
-          <input type='text' placeholder='Categoria' required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            {...register('description')}
+            type='text'
+            placeholder='Descrição'
+            required
+          />
+          <input
+            {...register('price', { valueAsNumber: true })}
+            type='number'
+            placeholder='Preço'
+            required
+          />
+          <input
+            {...register('category')}
+            type='text'
+            placeholder='Categoria'
+            required
+          />
 
           <TransactionType>
             <TransactionTypeButton value='income' variant='income'>
@@ -36,7 +77,9 @@ export function NewTransactionModal() {
               Saída
             </TransactionTypeButton>
           </TransactionType>
-          <button type='submit'>Cadastrar</button>
+          <button type='submit' disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,14 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { useSummary } from '../../hooks/useSummary';
+import { priceFormatter } from '../../utils/formatter';
 
 import { SummaryCard, SummaryContainer } from './styles';
 
 export function Summary() {
+  const summary = useSummary();
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -11,7 +17,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color='#00b37e' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -19,7 +25,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color='#f75a68' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant='green'>
         <header>
@@ -27,7 +33,7 @@ export function Summary() {
           <CurrencyDollar size={32} color='#fff' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
